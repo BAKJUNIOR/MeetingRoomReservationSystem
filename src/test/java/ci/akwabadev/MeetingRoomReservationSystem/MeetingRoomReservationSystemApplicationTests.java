51ac9668c57c050057dead589cc4a324c59dd4de
BAKJUNIOR <bakbassjunior@gmail.com>
2024-09-22 09:27:18 +0000

    Création & mise en place de architecture du projet

---
+++ src/test/java/ci/akwabadev/MeetingRoomReservationSystem/MeetingRoomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.akwabadev.MeetingRoomReservationSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetingRoomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
